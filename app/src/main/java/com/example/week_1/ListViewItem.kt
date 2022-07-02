89ae8da12eb68abd2ae4acdc288d29e776125d67
yuseongnam <dbtjd6511@gmail.com>
2022-07-02 10:52:23 +0900

    new branch for tab 3

--- app/src/main/java/com/example/week_1/ListViewItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.week_1
-
-import android.graphics.drawable.Drawable
-import org.json.JSONArray
-
-data class ListViewItem(var name: String, var nickname: String, var info: String, var favorite: JSONArray, var email: String, var phone: String )
-

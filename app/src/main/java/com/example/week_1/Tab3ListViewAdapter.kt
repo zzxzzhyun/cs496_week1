4b8bb7cd46aacd12e99ab55730ca6f0527966aff
yuseongnam <dbtjd6511@gmail.com>
2022-07-05 15:55:15 +0900

    distance data access

--- app/src/main/java/com/example/week_1/Tab3ListViewAdapter.kt
+++ app/src/main/java/com/example/week_1/Tab3ListViewAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.week_1
 
 import android.annotation.SuppressLint
+import android.location.Location
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import org.xml.sax.helpers.LocatorImpl
+import kotlin.math.round
 
-class Tab3ListViewAdapter(private val items: MutableList<Restaurant>): BaseAdapter(), Filterable {
+class Tab3ListViewAdapter(private val items: MutableList<Restaurant>, private val loc: Location?): BaseAdapter(), Filterable {
 
     var TAG = "Tab3ListViewAdapter"
 
@@ -31,6 +34,7 @@ class Tab3ListViewAdapter(private val items: MutableList<Restaurant>): BaseAdapt
         var name : TextView? = convertView?.findViewById<TextView>(R.id.tab3_list_name)
         var category : TextView? = convertView?.findViewById<TextView>(R.id.tab3_list_category)
         var location: TextView? = convertView?.findViewById<TextView>(R.id.location)
+        var distance:TextView? = convertView?.findViewById(R.id.distance)
 
         var item: Restaurant = filteredStores[position]
 
@@ -44,7 +48,13 @@ class Tab3ListViewAdapter(private val items: MutableList<Restaurant>): BaseAdapt
             category.setText(item.category)
         }
         if (location != null) {
+
             location.setText(item.location)
+        }
+        if(distance != null && loc != null){
+            val dist = calDist(item.lat, item.lon, loc.latitude, loc.longitude)
+            Log.d("dist", dist.toString())
+            distance.setText("${round(dist / 100.0) / 10.0} km")
         }
         return convertView
 
@@ -88,5 +98,19 @@ class Tab3ListViewAdapter(private val items: MutableList<Restaurant>): BaseAdapt
             println(filterResults.values)
             notifyDataSetChanged()
         }
+    }
+
+    private fun calDist(lat1:Double, lon1:Double, lat2:Double, lon2:Double) : Long{
+        val EARTH_R = 6371000.0
+        val rad = Math.PI / 180
+        val radLat1 = rad * lat1
+        val radLat2 = rad * lat2
+        val radDist = rad * (lon1 - lon2)
+
+        var distance = Math.sin(radLat1) * Math.sin(radLat2)
+        distance = distance + Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radDist)
+        val ret = EARTH_R * Math.acos(distance)
+
+        return Math.round(ret) // 미터 단위
     }
 }
